f0064cd8e6c30944a5865aa9476813de36a3ca30
yushuaiqi <yushuaiqi@nstc.com.cn>
2022-08-25 16:07:37 +0800

    overtest

---
+++ src/main/java/com/genius/demo/controller/ExceptionController.java
@@ -0,0 +1,40 @@
+package com.genius.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class ExceptionController {
+
+    @RequestMapping("/outOfMemory")
+    public String outOfMemory(HttpServletRequest request) {
+        try {
+            List<User> list = new ArrayList<User>();
+            //- 循环创建的实例对象被list引用，所以无法被GC回收
+            for (int i = 0; ; i++) {
+                System.out.println("轮次：" + i);
+                list.add(new User(UUID.randomUUID().toString()));
+            }
+        } catch (Exception e) {
+
+        }
+        return "outOfMemory";
+    }
+
+    public static class User {
+        private String id;
+
+        public User(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+}
